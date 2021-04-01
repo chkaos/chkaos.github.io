6a3dd6b96d940379d88337efccf854822383fa3d
chkaos <elviszhao2016@gmail.com>
2021-04-01 17:46:51 +0800

    Site updated: 2021-04-01 17:46:43

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@ function loadMainJs($, moment, ClipboardJS, config) {
         }
     }
     adjustNavbar();
-    $(window).resize(adjustNavbar);
+    $(window).resize(debounce(adjustNavbar, 200));
 
     function toggleFold(codeBlock, isFolded) {
         const $toggle = $(codeBlock).find('.fold i');
@@ -208,4 +208,6 @@ function loadMathJax() { //加载mathjax
 
 $(document).ready(function () {
     loadMainJs(jQuery, window.moment, window.ClipboardJS, window.IcarusThemeSettings);
+
+    console.info('%c CHKAOS %c ARE YOU OK?', 'background: #000; color: #fff; font-weight:900;font-size:24px','background: rgb(254,154,0); color: #000; font-weight:900;font-size:24px');
 });
